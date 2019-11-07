0bee47246d4aeba71957b7e75faf059fbaf57e97
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-07 20:00:55 +0900

    add setupNavigation

--- IdeaMemo/app/src/main/java/com/amebaownd/pikohan_nwiatori/idea/MainActivity.kt
+++ IdeaMemo/app/src/main/java/com/amebaownd/pikohan_nwiatori/idea/MainActivity.kt
@@ -2,11 +2,37 @@ package com.amebaownd.pikohan_nwiatori.idea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.amebaownd.pikohan_nwiatori.idea.util.MyContext
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        MyContext.onCreateAppcalition(this)
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration=
+            AppBarConfiguration.Builder(R.id.listMemoFragment).build()
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
